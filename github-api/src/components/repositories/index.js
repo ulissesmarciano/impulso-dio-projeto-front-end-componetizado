86e87a170b6cb6edf8cbacc5fe19a40ff53fa9af
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-06-29 22:59:51 -0300

    Criação da estrutura básica

--- github-api/src/components/repositories/index.js
+++ github-api/src/components/repositories/index.js
@@ -1,22 +1,19 @@
 import React from "react";
-import * as S from "./styled"
+import * as S from "./styled";
 
 const Repositories = () => {
-    return (
-    <S.WrapperTabs
-        selectedTabClassName = "is-selected"
-        selectedTabPanelClassName="is-selected"
-        >
-            <S.WrapperTabList>
-                <S.WrapperTab>Repositories</S.WrapperTab>
-                <S.WrapperTab>Starred</S.WrapperTab>
-            </S.WrapperTabList>
-            <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
-
+    return <S.WrapperTabs
+    selectedTabClassName="is-selected"
+    selectedTabPanelClassName="is-selected
+    ">
+        <S.WrapperTabList>
+            <S.WrapperTab>Repositories</S.WrapperTab>
+            <S.WrapperTab>Starred</S.WrapperTab>
+        </S.WrapperTabList>    
+        <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
+        <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
 
     </S.WrapperTabs>
-    );
 };
 
 export default Repositories
--- github-api/src/components/repositories/styled.js
+++ github-api/src/components/repositories/styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 
-
 export const WrapperTabs = styled(Tabs)`
     font-size: 16px;
-    width: 50%;
+    width: 100%;
+    margin-top: 16px;
 `;
 
 export const WrapperTabList = styled(TabList)`
@@ -13,40 +13,38 @@ export const WrapperTabList = styled(TabList)`
     display: flex;
     margin: 0;
 `;
-
-WrapperTabList.tabsRole = "Tablist";
+WrapperTabList.tabsRole = "TabList";
 
 export const WrapperTab = styled(Tab)`
     border-radius: 16px;
     border: 1px solid #ccc;
-    padding: 8px;
+    padding: 16px;
     user-select: none;
     cursor: pointer;
+    z-index: 99999;
+    background-color: "#fff";
+    margin: 8px;
 
-    &:focus{
+    &:focus {
         outline: none;
     }
 
     &.is-selected {
-        border-top-left-radius: 16px;
-        border-top-right-radius: 16px;
-        border-bottom-left-radius: 0;
-        border-bottom-right-radius: 0;
-        border-bottom: 1px solid white;
+        box-shadow: 2px 1px 8px rgba(0, 0, 0, 0.2);
     }
 `;
-
-WrapperTabList.tabsRole = "Tab";
+WrapperTab.tabsRole = "Tab";
 
 export const WrapperTabPanel = styled(TabPanel)`
-    padding: 8px;
+    padding: 16px;
     border: 1px solid "#ccc";
-    box-shadow: 0 0 0.8em rgba(0, 0, 0, 0.5);
     display: none;
+    margin-top: -5px;
 
     &.is-selected {
         display: block;
     }
 `;
 
-WrapperTabList.tabsRole = "TabPanel";
+WrapperTabPanel.tabsRole = "TabPanel";
+
