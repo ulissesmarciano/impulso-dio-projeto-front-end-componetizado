2c5ce123b47d83f4346412588757c3678f243dfb
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-06-30 13:40:18 -0300

    Adicionando a aula 5

---
+++ github-api/src/components/repository-item/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RepositoryItem = () => {
+    return <div>
+        <h2>impulso-dio-projeto-front-end-compnetizado</h2>
+        <h4>full name:</h4>
+        <a href = "https://www.github.com/ulissesmarciano/" target= "_blank" rel = "noreferrer">ulissesmarciano/impulso-dio-projeto-front-end-compnetizado</a>
+    </div>;
+};
+
+export default RepositoryItem;
